d512149c2884dd424e08888c6f8f41f2044a0206
Jodinurf <jodinurfarkhani04@gmail.com>
2023-04-14 15:13:27 +0700

    selama i = 0 ;i < n; i++ jumlah comparisons akan bertambah, jika arr[i] = item (elemen yg dicari) maka akan menunjukkan tampilan "found at position"

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -43,3 +43,10 @@ void LinearSearch()
 		comparisons = 0;
 		for (i = 0;i < n;i++)	//Langkah 2,3, dan 4
 		{
+			comparisons++;
+			if (arr[i] == item)		//Langkah 5a found
+			{
+				cout << "\n" << item << " found at position " << (i + 1) << endl;
+				break;
+			}
+		}
