26a1799955e943dc1e40a6088c55712e3055a36a
Jodinurf <jodinurfarkhani04@gmail.com>
2023-04-14 15:06:12 +0700

    membuat kendali program do while dimana akan melakukan penginputan elemen yang diinginkan

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -33,5 +33,34 @@ void LinearSearch()
 	char ch;
 	int comparisons;	//Number of Comparisons
 
-	
+	do
+	{
+		// Accept the number to be searched
+		cout << "\nEnter the element you want to search : ";		//Langkah 1
+		int item;
+		cin >> item;
+
+		comparisons = 0;
+		for (i = 0;i < n;i++)	//Langkah 2,3, dan 4
+		{
+			comparisons++;
+			if (arr[i] == item)		//Lanfkah 5a found
+			{
+				cout << "\n" << item << " found at position " << (i + 1) << endl;
+				break;
+			}
+		}
+		if (i == n) //langkah 5B not found
+			cout << "\n" << item << " not found in the array ";
+		cout << "\nNumber of Comparisons : " << comparisons << endl;
+
+		cout << "\nContinue Search (Y/N) : ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'Y'));
+}
+
+int main() {
+	input();
+	LinearSearch();
+	return 0;
 }
